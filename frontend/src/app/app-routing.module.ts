35eb2f7f09ac7c022bb1d2f3527d205158a613c6
saber <1999ms@live.fr>
2024-01-21 18:53:53 +0100

    crash fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InterventionComponent } from './intervention/intervention.component';
 
+
 const routes: Routes = [
   { path: 'interventions', component: InterventionComponent }
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InterventionComponent } from './intervention/intervention.component';
-import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InterventionComponent,
+    InterventionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    [CommonModule],
-    InterventionComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
