5f3ba182c833fd41259933e8417df1c46e5fcf2e
saber <1999ms@live.fr>
2024-01-21 18:36:31 +0100

    model

---
+++ backend/models/Intervention.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const interventionSchema = new mongoose.Schema({
+    surgeon: String,
+    specialty:String,
+    anesthsiste:String,
+    nurse1: String,
+    nurse2:String,
+    roomNumber:Number,
+    typeIntervention:String
+},{collection:'opérations'});
+
+const Intervention = mongoose.model('Intervention', interventionSchema);
+
+module.exports = Intervention;
