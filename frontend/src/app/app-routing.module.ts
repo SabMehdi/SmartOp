9bd48883a213ad5886de28c3bc47bbf714fc3e86
saber <1999ms@live.fr>
2024-01-22 19:30:13 +0100

    all interventions component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InterventionComponent } from './intervention/intervention.component';
+import { AllInterventionsComponent } from './all-interventions/all-interventions.component';
 
 
 const routes: Routes = [
-  { path: 'interventions', component: InterventionComponent }
+  { path: 'interventions', component: InterventionComponent },
+  { path: 'all-interventions', component: AllInterventionsComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InterventionComponent } from './intervention/intervention.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AllInterventionsComponent } from './all-interventions/all-interventions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InterventionComponent
+    InterventionComponent,
+    AllInterventionsComponent
   ],
   imports: [
     BrowserModule,
